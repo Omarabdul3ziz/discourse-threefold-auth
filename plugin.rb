7634ea55015691077f1418e3d924ae18c0195c16
Omar <omarabdul3ziz@gmail.com>
2023-01-02 17:27:29 +0000

    init threefold auth

--- plugin.rb
+++ plugin.rb
@@ -1,12 +1,40 @@
 # frozen_string_literal: true
 
-# name: discourse-plugin-name
-# about: TODO
-# version: 0.0.1
-# authors: Discourse
-# url: TODO
-# required_version: 2.7.0
+# name: discourse-threefold-auth
+# about: Enable login via Threefold connect
+# version: 1.0.0
+# authors: ThreeFold Dev
+# url: https://github.com/Omarabdul3ziz/discourse-threefold-auth
 
-enabled_site_setting :plugin_name_enabled
+require_relative "lib/omniauth_threefold"
 
-after_initialize {}
+enabled_site_setting :threefold_auth_enabled
+
+register_svg_icon "fab-microsoft"
+
+class ::ThreefoldAuthenticator < ::Auth::ManagedAuthenticator
+  def name
+    "threefold"
+  end
+
+  def register_middleware(omniauth)
+    omniauth.provider :threefold,
+                      setup: lambda { |env|
+                        strategy = env["omniauth.strategy"]
+                        strategy.options[:client_id] = SiteSetting.threefold_auth_client_id
+                        strategy.options[
+                          :client_secret
+                        ] = SiteSetting.threefold_auth_client_secret
+                      }
+  end
+
+  def enabled?
+    SiteSetting.threefold_auth_enabled
+  end
+
+  def primary_email_verified?(auth_token)
+    true
+  end
+end
+
+auth_provider authenticator: ThreefoldAuthenticator.new, icon: "fab-microsoft"
